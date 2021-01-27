fdafbb34515ed85e27b220d5450333c3fec21376
Abhinay Reddy <abhinay@MacBook-Pro.local>
2021-01-27 17:03:11 -0500

    added borders to maze

--- maze/index.js
+++ maze/index.js
@@ -14,8 +14,11 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true
-});
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
 
-World.add(world, shape);
+World.add(world, walls);
